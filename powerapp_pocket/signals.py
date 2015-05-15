56548cc371fa9685ad57c21d60ccc248028e9510
Roman Imankulov <roman.imankulov@gmail.com>
2015-05-16 00:00:46 +0100

    Ensure the integration works with the updated powerapp API

--- powerapp_pocket/signals.py
+++ powerapp_pocket/signals.py
@@ -22,33 +22,36 @@ PROJECT_NAME = 'Pocket task list'
 def on_integration_save(sender, instance=None, created=None, **kw):
     # as integration saved, create a new personal project for it
     if created and instance.service_id == 'powerapp_pocket':
-        get_personal_project(instance.user, instance, PROJECT_NAME)
+        get_personal_project(instance, PROJECT_NAME)
 
 
 @receiver(AppConfig.signals.todoist_task_added)
 @receiver(AppConfig.signals.todoist_task_updated)
-def on_task_added_edited(sender, user=None, service=None,
-                         integration=None, obj=None, **kw):
-    project = get_personal_project(user, integration, PROJECT_NAME)
+def on_task_added_edited(sender, integration=None, obj=None, **kw):
+    logger.debug('HAS SIGNAL %r', sender)
+    project = get_personal_project(integration, PROJECT_NAME)
     if obj['project_id'] != project['id']:
         return
-    process_project(user, project)
+    process_project(integration, project)
 
 
 @AppConfig.periodic_task(datetime.timedelta(minutes=60))
-def periodic_update(user, integration):
-    user.api.items.sync()
-    project = get_personal_project(user, integration, PROJECT_NAME)
-    process_project(user, project)
+def periodic_update(integration):
+    integration.api.items.sync()
+    project = get_personal_project(integration, PROJECT_NAME)
+    process_project(integration, project)
 
 
-def process_project(user, project):
-    access_token_obj = AccessToken.objects.filter(user=user, client='pocket').first()
+def process_project(integration, project):
+    access_token_obj = AccessToken.objects.filter(
+        user=integration.user, client='pocket').first()
     if not access_token_obj:
-        logger.warning('Pocket token for %s not found' % user)
+        logger.warning('Pocket token for %s not found' % integration.user)
         return
 
-    for item in user.api.items.all(lambda i: i['project_id'] == project['id'] and not i['checked']):
+    filt = lambda i: i['project_id'] == project['id'] and not i['checked']
+    for item in integration.api.items.all(filt):
+        logger.debug('Pocket: processing %s' % item['content'])
         urls = extract_urls(item['content'])
         for url in urls:
             requests.post(POCKET_ADD_URL_ENDPOINT, data={
@@ -61,4 +64,4 @@ def process_project(user, project):
             logger.debug('Pocket: added URL %s', url)
         item.complete()
 
-    user.api.commit()
+    integration.api.commit()
