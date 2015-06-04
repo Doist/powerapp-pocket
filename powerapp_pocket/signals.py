26f4d88d00c692cd69997ec9624a419813c4b880
Roman Imankulov <roman.imankulov@gmail.com>
2015-06-04 19:03:26 +0100

    Pocket "add URL" event handler moved to Celery

--- powerapp_pocket/signals.py
+++ powerapp_pocket/signals.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-import requests
 from logging import getLogger
-from django.conf import settings
 from django.dispatch.dispatcher import receiver
 from .apps import AppConfig
-from powerapp.core.models.oauth import OAuthToken
-from powerapp.core.todoist_utils import get_personal_project, extract_urls
-from powerapp_pocket.views import POCKET_ADD_URL_ENDPOINT
-
+from .tasks import process_item
+from powerapp.core.todoist_utils import get_personal_project
 
 logger = getLogger(__name__)
 
@@ -20,26 +16,4 @@ PROJECT_NAME = 'Pocket task list'
 def on_task_added_edited(sender, integration=None, obj=None, **kw):
     project = get_personal_project(integration, PROJECT_NAME)
     if obj['project_id'] == project['id'] and not obj['checked']:
-        process_item(integration, obj)
-
-
-def process_item(integration, item):
-    access_token_obj = OAuthToken.objects.filter(
-        user=integration.user, client='pocket').first()
-    if not access_token_obj:
-        logger.warning('Pocket token for %s not found' % integration.user)
-        return
-
-    logger.debug('Pocket: processing %s' % item['content'])
-    urls = extract_urls(item['content'])
-    for url in urls:
-        requests.post(POCKET_ADD_URL_ENDPOINT, data={
-            'url': url.link,
-            'title': url.title or '',
-            'consumer_key': settings.POCKET_CONSUMER_KEY,
-            'access_token': access_token_obj.token,
-            'tags': 'todoist',
-        })
-        logger.debug('Pocket: added URL %s', url)
-    item.complete()
-    integration.api.commit()
+        process_item.delay(integration.id, obj['content'], obj['id'])
---
+++ powerapp_pocket/tasks.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import requests
+from logging import getLogger
+from django.conf import settings
+from powerapp.celery import app
+from powerapp.core.models import Integration, OAuthToken
+from powerapp.core.todoist_utils import extract_urls
+from powerapp_pocket.views import POCKET_ADD_URL_ENDPOINT
+
+
+
+logger = getLogger(__name__)
+
+
+@app.task(ignore_result=True)
+def process_item(integration_id, item_content, item_id):
+    try:
+        integration = Integration.objects.get(id=integration_id)
+    except Integration.DoesNotExist:
+        return
+
+    access_token_obj = OAuthToken.objects.filter(
+        user=integration.user, client='pocket').first()
+    if not access_token_obj:
+        logger.warning('Pocket token for %s not found' % integration.user)
+        return
+
+    logger.debug('Pocket: processing %s' % item_content)
+    urls = extract_urls(item_content)
+    for url in urls:
+        resp = requests.post(POCKET_ADD_URL_ENDPOINT, data={
+            'url': url.link,
+            'title': url.title or '',
+            'consumer_key': settings.POCKET_CONSUMER_KEY,
+            'access_token': access_token_obj.access_token,
+            'tags': 'todoist',
+        })
+        if resp.status_code == 401:
+            # delete access token, it's not valid anymore
+            logger.warning('Pocket token for %s invalid. Delete it' % integration.user)
+            access_token_obj.delete()
+            return
+
+        resp.raise_for_status()
+        logger.debug('Pocket: added URL %s', url)
+    with integration.api.autocommit():
+        integration.api.item_update(item_id, checked=True, in_history=True)
